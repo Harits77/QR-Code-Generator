93b7c38a0967adfb61a9ac5c0a31d1383c5e3004
Hariharan MS <113448386+Harits77@users.noreply.github.com>
2025-01-04 23:56:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,16 +22,16 @@ function App() {
   return (
     
     <div className='mx-auto w-1/2 mt-10 pb-10 md:border-2 border-black'>
-      <h1 className='my-10 text-3xl font-bold h1' >QR Code Generate</h1>
+      <h1 className='my-10 text-3xl font-bold h1' >QR Code Generator</h1>
       <div className='mx-auto'>
       {load && <p>loading...</p>}
       {img && <img src={img} alt='No result' className='pb-10 mx-auto'/>}
       </div>
       <div className='flex flex-col mx-auto w-1/2 form'>
-      <label htmlFor="data" className='text-xl mb-3'>Data</label>
+      <label htmlFor="data" className='text-xl mb-3'>Data </label>
       <input type="text" value={Data} onChange={(e)=>setData(e.target.value)} placeholder='enter the url' id='data' className='border-2 border-black p-2 pl-4'/>
 
-      <label htmlFor="size" className='text-xl mt-5 mb-3'>Size</label>
+      <label htmlFor="size" className='text-xl mt-5 mb-3'>Size (e.g., 150)</label>
       <input type="text" value={size} onChange={(e)=>setSize(e.target.value)} placeholder='enter the size' id='size'  className='border-2 border-black p-2 pl-4'/>
       </div>
       
